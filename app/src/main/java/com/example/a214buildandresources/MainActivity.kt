9af559967d1bead81c8b6af5338c907f7a3a88ca
lugovoyNikita <zenit27069621@mail.ru>
2020-02-28 13:01:56 +0300

    Made an app

--- app/src/main/java/com/example/a214buildandresources/MainActivity.kt
+++ app/src/main/java/com/example/a214buildandresources/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.a214buildandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = """
+            Flavor=${BuildConfig.FLAVOR}
+            BuildType=${BuildConfig.BUILD_TYPE}
+            VersionCode=${BuildConfig.VERSION_CODE}
+            VersionName=${BuildConfig.VERSION_NAME}
+            ApplicationId=${BuildConfig.APPLICATION_ID}
+        """.trimIndent()
     }
 }
